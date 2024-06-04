6edede1059470fa8952f09584de36f3b8a510a97
Łukasz Waszczak <lukasz.waszczak@camdivision.pl>
2024-06-04 14:13:02 +0200

    User registration with email verification

--- User/src/main/java/com/tripplanner/user/UserConfig.java
+++ User/src/main/java/com/tripplanner/user/UserConfig.java
@@ -1,8 +1,37 @@
 package com.tripplanner.user;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Properties;
 
 @Configuration
 public class UserConfig {
+
+		@Bean
+		public BCryptPasswordEncoder bCryptPasswordEncoder() {
+				return new BCryptPasswordEncoder();
+		}
+
+		@Bean
+		public JavaMailSender javaMailSender(){
+				JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+				mailSender.setHost("app.debugmail.io");
+				mailSender.setPort(25);
+
+				mailSender.setUsername("ca95a758-c9e8-481f-be8a-a3159c1e984f");
+				mailSender.setPassword("7198dc2a-5a32-49bd-bb28-d94273b99da5");
+
+				Properties props = mailSender.getJavaMailProperties();
+				props.put("mail.transport.protocol", "smtp");
+				props.put("mail.smtp.auth", "true");
+				props.put("mail.smtp.starttls.enable", "true");
+				props.put("mail.debug", "true");
+
+				return mailSender;
+		}
 }
--- User/src/main/java/com/tripplanner/user/UserRepository.java
+++ User/src/main/java/com/tripplanner/user/UserRepository.java
@@ -1,9 +1,18 @@
 package com.tripplanner.user;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 		Optional<User> findByEmail(String email);
+
+		@Transactional
+		@Modifying
+		@Query("UPDATE User a " +
+				"SET a.enabled = TRUE WHERE a.email = ?1")
+		int enableUser(String email);
 }
